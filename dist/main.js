eb136bbeb4911317d55159ea31f07de659744fff
Cristopher <cristopher29092005@gmail.com>
2024-03-10 11:42:08 -0600

    View Port

--- dist/main.js
+++ dist/main.js
@@ -10,7 +10,9 @@ async function bootstrap() {
     app.use(cors());
     app.use(express.json({ limit: '50mb' }));
     app.use(express.urlencoded({ limit: '50mb', extended: true }));
-    await app.listen(process.env.PORT);
+    await app.listen(process.env.PORT, function () {
+        console.log({ env: process.env.PORT });
+    });
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
