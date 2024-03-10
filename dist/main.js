7b082ec1c578d6f276dd9ef76fa2e490fd5a1fb7
Cristopher <cristopher29092005@gmail.com>
2024-03-10 11:31:15 -0600

    Variable de entorno puerto

--- dist/main.js
+++ dist/main.js
@@ -4,12 +4,13 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const express = require("express");
 async function bootstrap() {
+    require("dotenv").config();
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     const cors = require('cors');
     app.use(cors());
     app.use(express.json({ limit: '50mb' }));
     app.use(express.urlencoded({ limit: '50mb', extended: true }));
-    await app.listen(3000);
+    await app.listen(process.env.PORT);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from './app.module';
 import * as express from 'express';
 
 async function bootstrap() {
+  require("dotenv").config()
   const app = await NestFactory.create(AppModule);
   const cors = require('cors')
   app.use(cors())
   app.use(express.json({ limit: '50mb' })); // Configura el límite del cuerpo JSON
   app.use(express.urlencoded({ limit: '50mb', extended: true })); // Configura el límite del cuerpo de datos codificados
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
